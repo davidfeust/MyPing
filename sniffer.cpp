7b64e3b2d81a913747fa01ff047e955d8bcc647f
david <davidhodefi0@gmail.com>
2021-01-05 23:29:55 +0200

    ge

--- sniffer.cpp
+++ sniffer.cpp
@@ -1,46 +1,119 @@
 
-#include "headers.h"
-#include <pcap/pcap.h>
-#include <cstdio>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <arpa/inet.h>
-#include <errno.h>
-#include <sys/time.h> // gettimeofday()
-#include <netdb.h>
-#include <fcntl.h>
+void print_icmp_packet(unsigned char* Buffer , int Size)
+{
+    unsigned short iphdrlen;
 
-#include <pcap.h>
-#include <stdio.h>
+    struct iphdr *iph = (struct iphdr *)Buffer;
+    iphdrlen = iph->ihl*4;
 
-void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-    printf("Got a packet\n");
+    struct icmphdr *icmph = (struct icmphdr *)(Buffer + iphdrlen);
+
+    fprintf(logfile,"\n\n***********************ICMP Packet*************************\n");
+
+    print_ip_header(Buffer , Size);
+
+    fprintf(logfile,"\n");
+
+    fprintf(logfile,"ICMP Header\n");
+    fprintf(logfile,"   |-Type : %d",(unsigned int)(icmph->type));
+
+    if((unsigned int)(icmph->type) == 11)
+        fprintf(logfile,"  (TTL Expired)\n");
+    else if((unsigned int)(icmph->type) == ICMP_ECHOREPLY)
+        fprintf(logfile,"  (ICMP Echo Reply)\n");
+    fprintf(logfile,"   |-Code : %d\n",(unsigned int)(icmph->code));
+    fprintf(logfile,"   |-Checksum : %d\n",ntohs(icmph->checksum));
+    //fprintf(logfile,"   |-ID       : %d\n",ntohs(icmph->id));
+    //fprintf(logfile,"   |-Sequence : %d\n",ntohs(icmph->sequence));
+    fprintf(logfile,"\n");
+
+    fprintf(logfile,"IP Header\n");
+    PrintData(Buffer,iphdrlen);
+
+    fprintf(logfile,"UDP Header\n");
+    PrintData(Buffer + iphdrlen , sizeof icmph);
+
+    fprintf(logfile,"Data Payload\n");
+    PrintData(Buffer + iphdrlen + sizeof icmph , (Size - sizeof icmph - iph->ihl * 4));
+
+    fprintf(logfile,"\n###########################################################");
 }
 
-int main() {
-    pcap_t *handle;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    struct bpf_program fp;
-    char filter_exp[] = "ip proto icmp";
-    bpf_u_int32 net;
 
-    // Step 1: Open live pcap session on NIC with name eth3
-    handle = pcap_open_live("eth3", BUFSIZ, 1, 1000, errbuf);
 
-    // Step 2: Compile filter_exp into BPF psuedo-code
-    pcap_compile(handle, &fp, filter_exp, 0, net);
-    pcap_setfilter(handle, &fp);
 
-    // Step 3: Capture packets
-    pcap_loop(handle, -1, got_packet, NULL);
 
-    pcap_close(handle);   //Close the handle
-    return 0;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include "headers.h"
+//#include <pcap/pcap.h>
+//#include <cstdio>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <unistd.h>
+//#include <string.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <netinet/ip.h>
+//#include <netinet/ip_icmp.h>
+//#include <arpa/inet.h>
+//#include <errno.h>
+//#include <sys/time.h> // gettimeofday()
+//#include <netdb.h>
+//#include <fcntl.h>
+//
+//#include <pcap.h>
+//#include <stdio.h>
+//
+//void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+//    printf("Got a packet\n");
+//}
+//
+//int main() {
+//    pcap_t *handle;
+//    char errbuf[PCAP_ERRBUF_SIZE];
+//    struct bpf_program fp;
+//    char filter_exp[] = "ip proto icmp";
+//    bpf_u_int32 net;
+//
+//    // Step 1: Open live pcap session on NIC with name eth3
+//    handle = pcap_open_live("eth3", BUFSIZ, 1, 1000, errbuf);
+//
+//    // Step 2: Compile filter_exp into BPF psuedo-code
+//    pcap_compile(handle, &fp, filter_exp, 0, net);
+//    pcap_setfilter(handle, &fp);
+//
+//    // Step 3: Capture packets
+//    pcap_loop(handle, -1, got_packet, NULL);
+//
+//    pcap_close(handle);   //Close the handle
+//    return 0;
+//}
--- sniffer3.cpp
+++ sniffer3.cpp
@@ -1,107 +1,103 @@
 
-#include "headers.h"
-#include <pcap/pcap.h>
-//#include <cstdio>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <arpa/inet.h>
-#include <errno.h>
-#include <sys/time.h> // gettimeofday()
-#include <netdb.h>
-#include <fcntl.h>
-
-#include <pcap.h>
-#include <stdio.h>
-
-#include <sys/socket.h>
+#include<netinet/in.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<netinet/ip_icmp.h>
+#include<netinet/ip.h>
+#include<net/ethernet.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
 #include <linux/if_packet.h>
-#include <net/ethernet.h>
-#include <stdio.h>
+#include <unistd.h>
+
+void filter_icmp_and_print(unsigned char *buf);
 
 void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
     printf("Got a packet\n");
 }
 
-void icmp_print(char *packet){
-
-    ICMP_Packet *icmpPacket ;
-//    icmpPacket->
-
-}
+//
 //https://stackoverflow.com/questions/14837453/how-to-sniff-all-icmp-packets-using-raw-sockets
-//1 they have youer answer
 int main() {
     int PACKET_LEN = IP_MAXPACKET;
-    char buffer[PACKET_LEN];
+//    unsigned char buffer[PACKET_LEN];
     struct sockaddr saddr;
     struct packet_mreq mr;
-    printf("packet_mreq\n");
 
     // Create the raw socket
-    int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));//IPPROTO_ICMP
+//    int sock = socket(AF_INET, SOCK_RAW, htons(ETH_P_ALL));//IPPROTO_ICMP
+//    int sock = socket(AF_PACKET, SOCK_RAW, htons(IPPROTO_ICMP));//IPPROTO_ICMP
+//    int sock = socket(AF_INET , SOCK_RAW , IPPROTO_ICMP);
+    int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));//reciv
+//    int sock = socket(AF_INET , SOCK_RAW , IPPROTO_TCP);
+//    int sock = socket(PF_INET , SOCK_RAW , IPPROTO_TCP);
+
     printf("socket = %d\n", sock);
     if (sock == -1) {
         perror("sock Error");
         return 1;
     }
 
-//    // Turn on the promiscuous mode.
-    mr.mr_type = PACKET_MR_PROMISC;
-    if (setsockopt(sock, SOL_PACKET, PACKET_ADD_MEMBERSHIP, &mr, sizeof(mr)) == -1) {
-        perror("promiscuous mode failed\n");
-    }
-    printf("mr_type\n");
+    //promiscuous mode does not work in our machine
 
     // Getting captured packets
-int i =10;
-    while (i){
-        i++;
-        memset(&buffer, 0, PACKET_LEN);
-//        printf("memset\n");
+    while (1) {
 
+        unsigned char buffer[IP_MAXPACKET]={0};
+//        for (int j=0;)
+//        memset(&buffer, 0, PACKET_LEN);
+//        printf("memset\n");
         size_t sizeof_addres = sizeof(saddr);
         int data_size = recvfrom(sock, buffer, PACKET_LEN, 0, &saddr, (socklen_t *) &sizeof_addres);
-//        printf("recvfrom\n");
+//        int data_size = recvfrom(sock_raw , buffer , 65536 , 0 , &saddr , &saddr_size);
+
+//        printf("recvfrom=%d\n", data_size);
 
         if (data_size >= 0) {
-            printf("data_size\n");
 
+//            printf("Got one packet: \n");
 
-            printf("Got one packet: %s\n", buffer);
+            filter_icmp_and_print(buffer);
         }
 //        break;
     }
-
     close(sock);
     return 0;
 }
 
-/*printf("mail");
-pcap_t *handle;
-char errbuf[PCAP_ERRBUF_SIZE];
-struct bpf_program fp;
-char filter_exp[] = "ip proto icmp";
-bpf_u_int32 net;
+void filter_icmp_and_print(unsigned char *buf) {
 
-// Step 1: Open live pcap session on NIC with name eth3
-handle = pcap_open_live("eth3", BUFSIZ, 1, 1000, errbuf);
-printf("handle");
+    struct iphdr *iph = (struct iphdr *)(buf  + sizeof(struct ethhdr));
+    sockaddr_in temp;
+    if (iph->protocol == IPPROTO_ICMP) {
+        printf("\n\nICMP PACKET\n");
+        temp.sin_addr.s_addr = iph->saddr;
 
-// Step 2: Compile filter_exp into BPF psuedo-code
-pcap_compile(handle, &fp, filter_exp, 0, net);
-pcap_setfilter(handle, &fp);
-printf("pcap_compile");
+        unsigned int iphdrlen = iph->ihl * 4;
 
-// Step 3: Capture packets
-pcap_loop(handle, -1, got_packet, NULL);
-printf("pcap_loop");
+        printf("IP_SRC : %s\n", inet_ntoa(temp.sin_addr));
 
-pcap_close(handle);   //Close the handle
-return 0;*/
+        temp.sin_addr.s_addr = iph->daddr;
+        printf("IP_DST : %s\n", inet_ntoa(temp.sin_addr));
+
+
+        struct icmphdr *icmph = (struct icmphdr *) (buf + iphdrlen + sizeof(struct ethhdr));
+
+//        int type = (int)icmph->type;
+//        if (type == 8 ){
+//            printf("TYPE : %d \n", type);
+//        }
+        uint8_t type = icmph->type;
+        printf("TYPE : %d", type);
+        if (type == 8){
+            printf(" Echo request\n");
+        }
+        if (type == 0){
+            printf(" Echo reply\n");
+        }
+        printf("CODE : %d\n", icmph->code);
+
+    }
+
+}
